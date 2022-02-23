5d9f451dd7c7509c5709ff77f5ec808f9cc808aa
hollowmatt <hollowmatt@gmail.com>
2022-02-23 17:34:16 -0500

    fix color defs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Grid from './components/Grid';
 
 function App() {
   return (
-    <div className="p-4">
+    <div className="p-0 bg-ggray-200">
       <h1 className='text-3xl font-bold'>
         <Grid />
       </h1>
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,6 +1,6 @@
 function Cell() {
     return(
-        <div className="w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-lg font-bold rounded bg-white border-slate-200"></div>
+        <div className="w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-lg font-bold rounded bg-ggray-100 border-ggray-800"></div>
     );
 }
 
