dadb94ef2bd92a2b4700bf9ba32284237f4b8cd8
hollowmatt <hollowmatt@gmail.com>
2022-02-23 09:07:26 -0500

    setup row

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,13 @@
+import Cell from './Cell';
+
 function Row() {
+    const emptyCells = Array.from(Array(5));
+
     return(
-        <div>
-            <p>
-                Item
-            </p>
+        <div className='flex justify-center mb1'>
+            {emptyCells.map((_,i) => (
+                <Cell />
+            ))}
         </div>
     );
 }
