60a2c7ff5eb556476e4a3776c929092a43da366a
hollowmatt <hollowmatt@gmail.com>
2022-02-24 09:25:00 -0500

    put placeholder for keyboard in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Grid from './components/Grid';
+import Keyboard from './components/Keyboard';
 
 function App() {
   return (
     <div className="p-0 bg-ggray-200">
       <h1 className='text-3xl font-bold'>
         <Grid />
+        <Keyboard />
       </h1>
     </div>
   );
---
+++ src/components/Keyboard.js
@@ -0,0 +1,9 @@
+function Keyboard() {
+    return(
+        <div className="flex justify-center mb1 pb-6">
+            <p>Keyboard here</p>
+        </div>
+    );
+}
+
+export default Keyboard;
